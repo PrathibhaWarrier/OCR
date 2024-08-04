dde558fc5ca488075212917e97a271b2af309e6a
PrathibhaWarrier <95683066+PrathibhaWarrier@users.noreply.github.com>
2024-08-04 21:08:35 +0530

    Create evntComp_try.ts

---
+++ evntComp_try.ts
@@ -0,0 +1,185 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { DocFile, DocParameterDetails } from '../../../shared/interface';
+import { FormBuilder } from '@angular/forms';
+import { DeleteFile, SharedService } from '../../../services/shared/shared.service';
+import { map } from 'rxjs';
+import { HttpEventType } from '@angular/common/http';
+
+@Component({
+  selector: 'signaldb-event',
+  templateUrl: './event.component.html',
+  styleUrls: ['./event.component.scss'],
+})
+export class EventComponent implements OnInit {
+  @Input() data: any;
+  @Input() eventListDocs: DocParameterDetails = {
+    owner: 'EVENTLIST',
+    files: [],
+  };
+  @Input() eventFiles: any[] = [];
+  @Input() actionType = '';
+  uploadProgress: number = 0;
+  isUploading = false;
+  allowedExtensions = ['.sig', '.txt'];
+
+  constructor(private fb: FormBuilder, private shared: SharedService) {}
+
+  eventForm = this.fb.group({
+    event_list_comments: [''],
+    total_repetitions: [],
+    file_paths: [[]]  // Add this new form control
+  });
+
+  onFolderSelected(event: any) {
+    const items = event.target.files;
+    this.eventFiles = [];
+    this.processFiles(items);
+    this.uploadFiles(this.eventFiles);
+  }
+
+  processFiles(items: FileList) {
+    const paths = [];
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+      if (item.webkitRelativePath) {
+        const filePath = item.webkitRelativePath;
+        if (this.isAllowedExtension(item.name)) {
+          paths.push(filePath); // Collect file paths
+          this.eventFiles.push({
+            file: item,
+            path: filePath
+          });
+        }
+      }
+    }
+    console.log('Filtered Files:', this.eventFiles);
+    this.eventForm.patchValue({ file_paths: paths }); // Save file paths in the form control
+  }
+
+  isAllowedExtension(fileName: string): boolean {
+    const extension = fileName.slice(fileName.lastIndexOf('.')).toLowerCase();
+    return this.allowedExtensions.includes(extension);
+  }
+
+  uploadFiles(files: any[]) {
+    const formData = new FormData();
+    for (let index = 0; index < files.length; index++) {
+      formData.append('file', files[index].file);
+    }
+    this.isUploading = true;
+    this.shared
+      .bulkFileUpload(formData)
+      .pipe(
+        map((event: any) => {
+          switch (event.type) {
+            case HttpEventType.UploadProgress:
+              this.uploadProgress = 0;
+              event.loaded && event.total && (this.uploadProgress = Math.round((event.loaded * 100) / event.total));
+              break;
+            case HttpEventType.Response:
+              return event;
+          }
+        })
+      )
+      .subscribe((event: any) => {
+        if (typeof event === 'object') {
+          const files = event.body;
+          this.isUploading = false;
+          for (let x = 0; x < files.length; x++) {
+            this.eventListDocs.files.push({
+              file_name: files[x].file_name,
+              original_name: files[x].original_name,
+            });
+          }
+        }
+      }, (error: any) => {
+        console.error('Upload failed:', error);
+        this.isUploading = false;
+      });
+  }
+
+  onFileSelected(event: any, type: string) {
+    const files = event.target.files;
+    const filesArray = Array.from(files);
+    this.uploadFiles(filesArray);
+  }
+
+  downloadFile(dbName: string, original_name: string) {
+    this.shared.downloadFile(dbName).subscribe((result: any) => {
+      const url = window.URL.createObjectURL(result);
+      const a = document.createElement('a');
+      document.body.appendChild(a);
+      a.setAttribute('style', 'display: none');
+      a.href = url;
+      a.download = original_name;
+      a.click();
+      window.URL.revokeObjectURL(url);
+      a.remove();
+    });
+  }
+
+  deleteFile(type: string, i: number, file: DocFile) {
+    let fileObj: DeleteFile;
+    this.eventListDocs.files.splice(i, 1);
+  }
+
+  focus(event: any) {
+    console.log('**');
+  }
+
+  autoFill() {
+    for (const f in this.data) {
+      this.eventForm.patchValue({
+        [f]: this.data[f],
+      });
+    }
+  }
+
+  ngOnInit(): void {
+    this.autoFill();
+  }
+}
+
+
+..............mtml
+
+<div>
+  <form [formGroup]="eventForm">
+    <div class="event-list-column">
+      <div class="file-note-row">
+        <div class="note-row">
+          <!-- Additional notes or information can be added here -->
+        </div>
+        <div class="form-row-item file-upload-div">
+          <div class="file-upload-div-inner">
+            <div>Upload event information files</div>
+            <input type="file" webkitdirectory (change)="onFolderSelected($event)" multiple />
+          </div>
+        </div>
+      </div>
+
+      <div class="file-path">
+        <div *ngFor="let file of eventFiles">
+          {{ file.path }}
+        </div>
+      </div>
+
+      <div class="comments-row">
+        <quill-editor (onFocus)="focus($event)"
+          class="quill-editor-event-list"
+          [(ngModel)]="eventForm.value.event_list_comments"
+          style="min-height: 40px; height: auto"
+          formControlName="event_list_comments"
+          placeholder="Type comments here...">
+        </quill-editor>
+      </div>
+
+      <!-- Optionally, display file paths stored in the form -->
+      <div class="saved-file-paths">
+        <div *ngFor="let path of eventForm.value.file_paths">
+          {{ path }}
+        </div>
+      </div>
+    </div>
+  </form>
+</div>
